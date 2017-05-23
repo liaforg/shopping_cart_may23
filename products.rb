a13d2562137d5e07f18bfd950ceea71f2474fdba
Lia Forgione <liaforg@gmail.com>
2017-05-23 16:10:59 -0400

    shopping_cart aand products final

--- products.rb
+++ products.rb
@@ -1,23 +1,19 @@
 class Products
 
-  attr_accessor :name, :base_price, :tax
+  attr_accessor :name, :base_price
+  attr_reader :tax
 
   def initialize(name, base_price)
     @name = name
-    @price = base_price
+    @base_price = base_price
     @tax = 1.13
   end
 
   def total_cost
-    @total_cost = @price * @tax
+    @total_cost = @base_price * @tax
     @total_cost.round(2)
   end
 
 
-product1 = Products.new('apple', 1)
-product2 = Products.new('orange', 2)
-product3 = Products.new('lemon', 3)
-product4 = Products.new('banana', 4)
-product5 = Products.new('cherry', 5)
 
 end
--- shopping_cart.rb
+++ shopping_cart.rb
@@ -1,3 +1,41 @@
 require_relative 'products.rb'
 
 class Shopping_Cart
+  @@products = []
+
+  def initialize
+    @cost_before_tax = 0
+    @cost_after_tax  = 0
+  end
+
+  def self.all_products
+    @@products
+  end
+
+  def self.cost_before_tax
+    @cost_before_tax = 0
+    @@products.each do |product|
+    @cost_before_tax += product.base_price
+  end
+    @cost_before_tax
+  end
+
+ def self.cost_after_tax
+   @@products.each do |product|
+   @cost_after_tax = (product.base_price * @tax)
+  end
+   @cost_after_tax
+ end
+
+
+ def self.add_product(name,price)
+  new_product = Products.new(name,price)
+  @@products << new_product
+  new_product
+ end
+
+ def self.rem_product(inst)
+  @@products.delete(inst)
+ end
+
+end
